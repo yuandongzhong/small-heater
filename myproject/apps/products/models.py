1ce7dd2c7655d1a83f8f545aec0b9fb5d5000351
yuandongzhong <patrick_d@foxmail.com>
2019-10-17 22:27:39 +0800

    added __string__ for Category

--- myproject/apps/products/models.py
+++ myproject/apps/products/models.py
@@ -10,6 +10,9 @@ class Category(models.Model):
         verbose_name = 'category'
         verbose_name_plural = 'categories'
 
+    def __str__(self):
+        return self.title
+
 
 class Product(models.Model):
     title = models.CharField(max_length=100, blank=True)
